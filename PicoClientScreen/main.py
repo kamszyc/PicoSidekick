aa7e2b42d0c1165597744726682e3cde825c59fe
Kamil Szyc <kamszyc@gmail.com>
2025-03-21 20:05:00 +0100

    Settings page, devmode and shutdown buttons put there

--- PicoClientScreen/main.py
+++ PicoClientScreen/main.py
@@ -22,7 +22,7 @@ from adafruit_displayio_layout.layouts.page_layout import PageLayout
 SHUTDOWN_BUTTON_TEXT = "SHUTDOWN"
 CONFIRMATION_TEXT = "SURE?"
 
-async def handle_touch(touch_context, play_button, shutdown_button, devmode_button):
+async def handle_touch(touch_context, page_layout, play_button, shutdown_button, devmode_button, settings_button, back_button):
     cc = ConsumerControl(usb_hid.devices)
     shutdown_pressed = False
     while True:
@@ -30,7 +30,7 @@ async def handle_touch(touch_context, play_button, shutdown_button, devmode_butt
         if curTick >= touch_context.NxTick:
             touch_context.NxTick = curTick + touch_context.taskInterval_50ms
             touch_context.touch_det_task()
-            
+
         #handle touch event
         if touch_context.touchEvent != EVT_NO:
             if touch_context.touchEvent == EVT_PenDown:
@@ -43,7 +43,15 @@ async def handle_touch(touch_context, play_button, shutdown_button, devmode_butt
                     print('devmode on/off')
                     toggle_devmode()
                     microcontroller.reset()
+
                 
+                if page_layout.showing_page_name == "settings_page" and back_button.contains((touch_context.touchedY, touch_context.touchedX)):
+                    print('back')
+                    page_layout.show_page("main_page")
+                elif settings_button.contains((touch_context.touchedY, touch_context.touchedX)):
+                    print('settings')
+                    page_layout.show_page("settings_page")
+
                 if play_button.contains((touch_context.touchedY, touch_context.touchedX)):
                     print('play/pause')
                     cc.send(ConsumerControlCode.PLAY_PAUSE)
@@ -74,7 +82,14 @@ def create_idle_page():
     idle_page_group.append(lbl1)
     return idle_page_group
 
-async def render_display(play_button, shutdown_button, devmode_button):
+def create_settings_page(shutdown_button, devmode_button, back_button):
+    settings_page_group = displayio.Group()
+    settings_page_group.append(shutdown_button)
+    settings_page_group.append(devmode_button)
+    settings_page_group.append(back_button)
+    return settings_page_group
+
+async def render_display(page_layout, play_button, shutdown_button, devmode_button, settings_button, back_button):
     displayio.release_displays()
 
     tft_spi_clk = board.GP6
@@ -97,13 +112,14 @@ async def render_display(play_button, shutdown_button, devmode_button):
     root = displayio.Group()
     display.root_group = root
     
-    page_layout = PageLayout(x=0, y=0)
     root.append(page_layout)
 
     main_group = displayio.Group()
     idle_group = create_idle_page()
+    settings_group = create_settings_page(shutdown_button, devmode_button, back_button)
     page_layout.add_content(idle_group, "idle_page")
     page_layout.add_content(main_group, "main_page")
+    page_layout.add_content(settings_group, "settings_page")
 
     IDLE_MUSIC = "Play some music!"
 
@@ -135,8 +151,7 @@ async def render_display(play_button, shutdown_button, devmode_button):
 
     main_group.append(text_group)
     main_group.append(play_button)
-    main_group.append(shutdown_button)
-    main_group.append(devmode_button)
+    main_group.append(settings_button)
 
     iterations_without_update = 0
     while True:
@@ -170,7 +185,8 @@ async def render_display(play_button, shutdown_button, devmode_button):
 
                 time_label.text = request["time"]
 
-                page_layout.show_page("main_page")
+                if page_layout.showing_page_name == "idle_page":
+                    page_layout.show_page("main_page")
             except Exception as e:
                 print(e)
             iterations_without_update = 0
@@ -202,14 +218,18 @@ def create_button(x, y, label):
 async def main():
     touch_context = TouchContext()
     
+    page_layout = PageLayout(x=0, y=0)
+
     play_button = create_button(125, 95, "PLAY")
-    shutdown_button = create_button(240, 180, SHUTDOWN_BUTTON_TEXT)
+    settings_button = create_button(240, 180, "SETTINGS")
 
+    shutdown_button = create_button(135, 90, SHUTDOWN_BUTTON_TEXT)
+    back_button = create_button(135, 170, "BACK")
     devmode_text = "DEVMODE OFF" if devmode_enabled() else "DEVMODE ON"
-    devmode_button = create_button(10, 2, devmode_text)
+    devmode_button = create_button(135, 10, devmode_text)
 
-    handle_touch_task = asyncio.create_task(handle_touch(touch_context, play_button, shutdown_button, devmode_button))
-    render_display_task = asyncio.create_task(render_display(play_button, shutdown_button, devmode_button))
+    handle_touch_task = asyncio.create_task(handle_touch(touch_context, page_layout, play_button, shutdown_button, devmode_button, settings_button, back_button))
+    render_display_task = asyncio.create_task(render_display(page_layout, play_button, shutdown_button, devmode_button, settings_button, back_button))
     await asyncio.gather(handle_touch_task, render_display_task)
 
 asyncio.run(main())
